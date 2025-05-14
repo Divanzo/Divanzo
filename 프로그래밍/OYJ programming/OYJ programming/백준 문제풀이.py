bbfec4285dc93af5c94582034a702bf0bdc1505d
Divanzo <dhdydwns723@gmail.com>
2025-05-14 19:35:55 +0900

    feat :: OYJ programming

---
+++ 프로그래밍/OYJ programming/OYJ programming/백준 문제풀이.py
@@ -0,0 +1,126 @@
+# n=int(input())
+
+# result=0
+
+# alist=list(map(int, input().split()))
+
+# for i in range(len(alist)):
+#     result+=alist[i]/max(alist)*100
+
+# print(result/n)
+#==================================================================
+# word=input()
+# n=int(input())
+# alist=[]
+
+# for i in word:
+#     alist.append(i)
+
+# print(alist[n-1])
+
+# n=int(input())
+
+# word=input()
+# sum1=0
+
+# for i in word:
+#     sum1+=int(i)
+
+# print(sum1)
+#=================================================================
+# alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# s=input()
+
+# for i in alpha:
+#     if i in s:
+#         print(s.index(i),end=' ')
+#     else:
+#         print(-1,end=' ')
+#=================================================================
+# n=int(input())
+
+# for i in range(n):
+#     alist=[]
+#     num_word=list(map(str, input().split()))
+
+#     num_word[0]=int(num_word[0])
+
+#     for j in num_word[1]:
+#         for k in range(num_word[0]):
+#             alist.append(j)
+#     for j in alist:
+#         print(j,end='')
+#     print()
+#=================================================================
+# word=input().split()
+
+# print(len(word))
+
+# word=input().split()
+
+# for i in range(len(word)):
+#     word[i]=int(word[i][::-1])
+
+# print(max(word))
+#=================================================================
+# diar=[['none'],['none'],[1],[2,'A','B','C'],[3,'D','E','F'],[4,'G','H','I'],[5,'J','K','L'],[6,'M','N','O'],[7,'P','Q','R','S'],[8,'T','U','V'],[9,'W','X','Y','Z'],[0]]
+# word=input()
+# sum1=0
+# for i in word:
+#     for j in diar:
+#         if i in j:
+#             sum1+=diar.index(j)
+
+# print(sum1)
+#=================================================================
+# import sys
+
+# for line in sys.stdin:
+#     print(line.strip())
+
+# info=[1,1,2,2,2,8]
+# Check=[]
+
+# alist=list(map(int, input().split()))
+
+# for i in range(len(info)):
+#     if info[i]==alist[i]:
+#         Check.append(0)
+#     elif info[i]>alist[i] or info[i]<alist[i]:
+#         Check.append(info[i]-alist[i])
+
+# for i in Check:
+#     print(i,end=' ')
+#=================================================================
+# a=int(input())
+
+# for i in range(a):
+#     for j in range(a-i-1,0,-1):
+#         print(" ", end="")
+#     for j in range(2*i+1):
+#         print("*", end="")
+#     print()
+
+# for i in range(a-1):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for j in range(2*(a-i-1)-1):
+#         print("*", end="")
+#     print()
+#=================================================================
+# n=list(input())
+# sum1=1
+
+# for i in range(len(n)//2):
+#     if n[i]!=n[-(i+1)]:
+#         sum1=0
+#         break
+# print(sum1)
+
+sen=list(input())
+Check=[0]*len(set(sen))
+
+
+
+for i in Check:
+    print(i, end=' ')
